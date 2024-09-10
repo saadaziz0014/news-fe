ca910f95642f7a6c8270412f5215b9d100a1272b
Muhammad Saad Aziz <80111445+saadaziz0014@users.noreply.github.com>
2024-09-10 13:12:10 +0500

    complete project

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+export default function Loading() {
+    return (
+        <>
+            <div className="loading">
+                <h1 className="loading-header">Loading...</h1>
+            </div>
+        </>
+    )
+}
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -4,23 +4,32 @@ import Button from "./Button";
 import News from "./News";
 import { useCallback } from 'react';
 import axios from 'axios';
+import Loading from './Loading';
 
 export default function NewsList() {
     const [news, setNews] = useState()
+    const [loading, setLoading] = useState(false)
     const getData = useCallback(async () => {
-        let data = await axios.get(import.meta.env.VITE_API_URL + '/scrape');
-        setNews(data.data)
+        setLoading(true);
+        let data = await axios.get(import.meta.env.VITE_API_URL + 'scrape');
+        setNews(data.data);
+        setLoading(false);
     }, [news, setNews])
-    return (
-        <>
-            <div className="news-button">
-                <Button text="Load Data" onClick={getData} />
-            </div>
-            <div className="news-list">
-                <News title="Title 1" url="https://example.com" text="Lorem ipsum" />
-                <News title="Title 2" url="https://example.com" text="Lorem ipsum" />
-                <News title="Title 3" url="https://example.com" text="Lorem ipsum" />
-            </div>
-        </>
-    )
+    if (loading) {
+        return <Loading />
+    }
+    else {
+        return (
+            <>
+                <div className="news-button">
+                    <Button text="Load Data" onClick={getData} />
+                </div>
+                <div className="news-list">
+                    {news ? news.map((item, index) => (
+                        <News key={index} title={item.title} url={item.url} text={item.text} />
+                    )) : <h1>No News... Click Load Data</h1>}
+                </div>
+            </>
+        )
+    }
 }

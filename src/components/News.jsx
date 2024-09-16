797a3b47947b6b5d82c555db9ad336cc425803e4
Muhammad Saad Aziz <80111445+saadaziz0014@users.noreply.github.com>
2024-09-16 11:29:02 +0500

    change style

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,10 +3,8 @@ import '../App.css'
 export default function News({ title, url, text }) {
     return (
         <div className="news">
-            <h3>{title}</h3>
-            <p>{text}</p>
             <a href={url} target="_blank" rel="noreferrer">
-                {url}
+                <h3>{title}</h3>
             </a>
         </div>
     )
